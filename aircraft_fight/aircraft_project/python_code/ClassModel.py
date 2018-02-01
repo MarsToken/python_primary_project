a6ff3e00f0f4faa2ce459a3a2d280b0547a13fe9
wangmaobo <wangmaobo@51yjtc.com>
2018-02-01 18:00:14 +0800

    add score and death's animation

--- aircraft_fight/aircraft_project/python_code/ClassModel.py
+++ aircraft_fight/aircraft_project/python_code/ClassModel.py
@@ -3,8 +3,32 @@
 # function:各种类数据模型
 import random
 import pygame
-import time
 import aircraft_fight.aircraft_project.python_code.Constants as Constants
+
+type_image_enemy = ('../feiji/enemy0.png',
+                    '../feiji/enemy1.png',
+                    '../feiji/enemy2.png')
+list_image_death_aim = (('../feiji/enemy0_down1.png',
+                         '../feiji/enemy0_down2.png',
+                         '../feiji/enemy0_down3.png',
+                         '../feiji/enemy0_down4.png'),
+                        ('../feiji/enemy1_down1.png',
+                         '../feiji/enemy1_down2.png',
+                         '../feiji/enemy1_down3.png',
+                         '../feiji/enemy1_down4.png'),
+                        ('../feiji/enemy2_down1.png',
+                         '../feiji/enemy2_down2.png',
+                         '../feiji/enemy2_down3.png',
+                         '../feiji/enemy2_down4.png'))
+type_size = ({'width': Constants.EMENY0_WIDTH,
+              'height': Constants.EMENY0_HEIGHT},
+             {'width': Constants.EMENY1_WIDTH,
+              'height': Constants.EMENY1_HEIGHT},
+             {'width': Constants.EMENY2_WIDTH,
+              'height': Constants.EMENY2_HEIGHT})
+type_image_enemy_bullet = ('../feiji/bullet1.png',
+                           '../feiji/bullet2.png',
+                           '../feiji/bullet.png')
 
 
 class BasePlane:
@@ -12,7 +36,7 @@ class BasePlane:
     list_enemy_plant = []
     list_player_plant = []
 
-    def __init__(self, screen, x, y, w, h, image_name):
+    def __init__(self, screen, x, y, w, h, image_name, identify):
         self.x = x
         self.y = y
         self.width = w
@@ -23,6 +47,7 @@ class BasePlane:
         self.main_time = 0  # 主线程循环次数
         self.death_index = 0  # 死亡动画帧数
         self.is_dead = False
+        self.identify = identify  # 身份，1为英雄 其他为敌机
 
     def display(self):
         # 如果死了就不用判断了，直到死亡动画结束
@@ -46,18 +71,23 @@ class BasePlane:
             for _enemy in BasePlane.list_enemy_plant:
                 _enemy_left = _enemy.x
                 _enemy_top = _enemy.y
-                _enemy_right = _enemy.x + Constants.EMENY0_WIDTH
-                _enemy_bottom = _enemy.y + Constants.EMENY0_HEIGHT
+                _enemy_right = _enemy.x + type_size[_enemy.type]['width']
+                _enemy_bottom = _enemy.y + type_size[_enemy.type]['height']
                 if _player_left < _enemy_right and _enemy_left < _player_right and _player_top < _enemy_bottom and _enemy_top < _player_bottom:
                     print("敌我相撞")
-                    self.is_dead = True
+                    _player.is_dead = True
+                    _enemy.is_dead = True
                     return True
 
     def fire(self):
         pass
 
     def dead(self, list_image):
-        self.screen.blit(pygame.image.load(list_image[self.death_index]), (self.x, self.y))
+        if self.identify == 1:
+            self.screen.blit(pygame.image.load(list_image[self.death_index]), (self.x, self.y))
+        else:
+            self.screen.blit(pygame.image.load(list_image[self.type][self.death_index]),
+                             (self.x, self.y))
         self.main_time += 1
         if self.main_time == 5:  # 每个爆炸图片显示5*0.01s，太长会有bug_is_hint：true-false
             self.main_time = 0
@@ -65,7 +95,14 @@ class BasePlane:
         if self.death_index > 3:
             self.death_index = 0
             self.is_dead = False
-            time.sleep(1)
+            if self in BasePlane.list_player_plant:
+                BasePlane.list_player_plant.remove(self)
+            list_temp = []
+            if self in BasePlane.list_enemy_plant:
+                list_temp.append(self)
+            for temp in list_temp:
+                BasePlane.list_enemy_plant.remove(temp)
+            # time.sleep(1)
             # exit()
 
 
@@ -76,23 +113,24 @@ class Player(BasePlane):
                          Constants.INITIAL_PLATER_Y,
                          Constants.PLAYER_WIDTH,
                          Constants.PLAYER_HEIGHT,
-                         '../feiji/hero1.png')
+                         '../feiji/hero1.png', 1)
         BasePlane.list_player_plant.append(self)
+        self.score = 0
 
     def move_left(self):
-        self.x -= Constants.UNIT_LEN
+        self.x -= Constants.UNIT_LEN * Constants.PLAYER_MOVE_FACTOR
         self.__move_beyond()
 
     def move_right(self):
-        self.x += Constants.UNIT_LEN
+        self.x += Constants.UNIT_LEN * Constants.PLAYER_MOVE_FACTOR
         self.__move_beyond()
 
     def move_top(self):
-        self.y -= Constants.UNIT_LEN
+        self.y -= Constants.UNIT_LEN * Constants.PLAYER_MOVE_FACTOR
         self.__move_beyond()
 
     def move_bottom(self):
-        self.y += Constants.UNIT_LEN
+        self.y += Constants.UNIT_LEN * Constants.PLAYER_MOVE_FACTOR
         self.__move_beyond()
 
     # 越界判断
@@ -115,7 +153,7 @@ class Player(BasePlane):
             for bullet in enemy_plane.bullet_list:
                 x = bullet.x
                 y = bullet.y
-                if self.x < x < self.x + self.width and self.y < y < self.y + self.width:
+                if self.x < x < self.x + self.width and self.y < y < self.y + self.height:
                     self.is_dead = True
                     return True
         return False
@@ -126,20 +164,21 @@ class Player(BasePlane):
                       '../feiji/hero_blowup_n3.png',
                       '../feiji/hero_blowup_n4.png']
         super().dead(list_image)
-        if self in BasePlane.list_player_plant:
-            BasePlane.list_player_plant.remove(self)
-            self = None
+        # 死了之后清空对象clear
+        # self = None
 
 
 class EnemyPlane(BasePlane):
-    def __init__(self, screen):
+    def __init__(self, screen, type):
+        # 根据类型，定敌机的各种参数，比如图片，宽高尺寸等
         super().__init__(screen,
                          Constants.COORDINATE_X,
                          Constants.COORDINATE_Y,
-                         Constants.EMENY0_WIDTH,
-                         Constants.EMENY0_HEIGHT,
-                         '../feiji/enemy0.png')
+                         type_size[type]['width'],
+                         type_size[type]['height'],
+                         type_image_enemy[type], -1)
         self.direction = 'right'
+        self.type = type  # 敌机类型
         BasePlane.list_enemy_plant.append(self)
         print("1=======%d" % len(BasePlane.list_enemy_plant))
 
@@ -148,42 +187,44 @@ class EnemyPlane(BasePlane):
         self.__lose()
 
     def move_free(self):
-        self.y += Constants.UNIT_LEN / 4
+        self.y += Constants.UNIT_LEN * Constants.ENEMY_MOVE_FACTOR
         if self.direction == 'right':
-            self.x += (Constants.UNIT_LEN / 4)
+            self.x += (Constants.UNIT_LEN * Constants.ENEMY_MOVE_FACTOR)
         elif self.direction == 'left':
-            self.x -= (Constants.UNIT_LEN / 4)
-        if self.x > Constants.BACKGROUND_WIDTH - Constants.EMENY0_WIDTH:
+            self.x -= (Constants.UNIT_LEN * Constants.ENEMY_MOVE_FACTOR)
+        if self.x > Constants.BACKGROUND_WIDTH - type_size[self.type]['width']:
             self.direction = 'left'
-            self.x = Constants.BACKGROUND_WIDTH - Constants.EMENY0_WIDTH
+            self.x = Constants.BACKGROUND_WIDTH - type_size[self.type]['width']
         elif self.x < 0:
             self.direction = 'right'
             self.x = 0
 
     def fire(self):
-        rad = random.randint(0, 100)
+        rad = random.randint(0, Constants.BASE_POSSIBILITY * Constants.ENEMY_DENSITY)
         if rad == 0:
-            self.bullet_list.append(EnemyBullet(self.screen, self.x, self.y))
+            self.bullet_list.append(EnemyBullet(self.screen, self.type, self.x, self.y))
 
     def is_hint(self):
         super().is_hint()
-        for enemy_plane in BasePlane.list_player_plant:
-            for bullet in enemy_plane.bullet_list:
+        for player in BasePlane.list_player_plant:
+            for bullet in player.bullet_list:
                 x = bullet.x
                 y = bullet.y
-                if self.x < x < self.x + self.width and self.y < y < self.y + self.width:
+                if self.x < x < self.x + self.width and self.y < y < self.y + self.height:
                     self.is_dead = True;
+                    if self.type == 0:
+                        player.score += Constants.ENEMY0_DEATH_SCORE
+                    elif self.type == 1:
+                        player.score += Constants.ENEMY1_DEATH_SCORE
+                    elif self.type == 2:
+                        player.score += Constants.ENEMY2_DEATH_SCORE
+                    # 击中敌机，我的子弹就消失
+                    player.bullet_list.remove(bullet)
                     return True
         return False
 
     def dead(self):
-        list_image = ['../feiji/enemy0_down1.png',
-                      '../feiji/enemy0_down2.png',
-                      '../feiji/enemy0_down3.png',
-                      '../feiji/enemy0_down4.png']
-        super().dead(list_image)
-        if self in BasePlane.list_enemy_plant:
-            BasePlane.list_enemy_plant.remove(self)
+        super().dead(list_image_death_aim)
 
     def __lose(self):
         if self.y > Constants.BACKGROUND_HEIGHT:
@@ -213,7 +254,7 @@ class PlayerBullet(BaseBullet):
         super().__init__(screen, x + 40, y - 20, '../feiji/bullet.png')
 
     def move(self):
-        self.y -= Constants.UNIT_LEN / 2
+        self.y -= Constants.UNIT_LEN * Constants.PLAYER_BULLET_MOVE_FACTOR
 
     def isBeyond(self):
         if self.y < 0:
@@ -223,14 +264,14 @@ class PlayerBullet(BaseBullet):
 
 
 class EnemyBullet(BaseBullet):
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, type, x, y):
         super().__init__(screen,
-                         x + Constants.EMENY0_WIDTH / 2,
-                         y + Constants.EMENY0_HEIGHT,
-                         '../feiji/bullet1.png')
+                         x + type_size[type]['width'] / 2,
+                         y + type_size[type]['height'],
+                         type_image_enemy_bullet[type])
 
     def move(self):
-        self.y += Constants.UNIT_LEN / 2
+        self.y += Constants.UNIT_LEN * Constants.ENEMY_BULLET_MOVE_FACTOR
 
     def isBeyond(self):
         if self.y > Constants.BACKGROUND_HEIGHT - Constants.EMEMY0_BULLET_HEIGHT:
--- aircraft_fight/aircraft_project/python_code/Main.py
+++ aircraft_fight/aircraft_project/python_code/Main.py
@@ -6,6 +6,7 @@ import time
 from pygame.locals import *
 import aircraft_fight.aircraft_project.python_code.ClassModel as model
 import aircraft_fight.aircraft_project.python_code.Constants as Contants
+import random
 
 
 def detect_keyboard(player):  # 监听键盘
@@ -33,16 +34,23 @@ def main():
     screen = pygame.display.set_mode((Contants.BACKGROUND_WIDTH, Contants.BACKGROUND_HEIGHT), 0, 32)
     background = pygame.image.load('../feiji/background.png')
     player = model.Player(screen)
-
-    enemy_0 = model.EnemyPlane(screen)
-
+    # 分数显示 初始化
+    pygame.init()
+    font = pygame.font.SysFont('arial', 16)
     while True:
+        # random随机生成飞机对象
+        result = random.randint(0, Contants.BASE_POSSIBILITY * Contants.ENEMY_DENSITY)
+        if result == 0 or result == 1 or result == 2:
+            model.EnemyPlane(screen, result)
         # 背景，坐标
         screen.blit(background, (Contants.COORDINATE_X, Contants.COORDINATE_Y))
+        for emery in model.BasePlane.list_enemy_plant:
+            emery.display()
+            emery.move_free()
+            emery.fire()
         player.display()
-        enemy_0.display()
-        enemy_0.move_free()
-        enemy_0.fire()
+        name_surface = font.render('score:%d' % player.score, True, (0, 0, 0), (255, 255, 255))
+        screen.blit(name_surface, (10, 10))
         # 更新
         pygame.display.update()
         detect_keyboard(player)
